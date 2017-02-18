de353ad7d738a11e93f042825efdfa5161a95374
chouxi <qizheng1993hit@gmail.com>
2017-02-18 16:57:14 -0500

    knn implementation

--- test.py
+++ test.py
@@ -25,10 +25,30 @@ def test_features(mean, std, file_name):
     normalize(features_list, mean, std)
     return [features_list, res[1]]
 
+def k_nn(D_index, char_index, k):
+    prediction = [{}]
+    for j in range(k):
+        for i in range(len(D_index[:,j])):
+            cur_char = index_2_char(D_index[i][j],char_index)
+            if len(prediction) == i:
+                prediction.append({})
+                prediction[i].setdefault(cur_char, 1)
+            else:
+                if prediction[i].has_key(cur_char):
+                    prediction[i][cur_char] += 1
+                else:
+                    prediction[i].setdefault(cur_char, 1)
+    result = []
+    for d in prediction:
+        print d
+        tmp = max(d.items(), key=lambda x: x[1])
+        print tmp
+        if tmp[1] == 1:
+            result.append(d.keys()[0])
+        else:
+            result.append(tmp[0])
+    return result
 def recognition(test_features, data_base, char_index):
     D = cdist(test_features, data_base)
     D_index = np.argsort(D, axis=1)
-    prediction = []
-    for i in range(len(D_index[:,0])):
-        prediction.append(index_2_char(D_index[i][0],char_index))
-    return prediction
+    return k_nn(D_index, char_index, 10)
